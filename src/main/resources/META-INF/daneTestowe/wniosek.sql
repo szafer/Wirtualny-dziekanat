cdc63ade8228d294ffdc08fc1c815df9670f0fd2
HalinaKopicera <halina.kopicera@gmail.com>
2018-01-11 16:47:20 +0100

    uzupełnienie struktury

--- src/main/resources/META-INF/daneTestowe/wniosek.sql
+++ src/main/resources/META-INF/daneTestowe/wniosek.sql
@@ -1,4 +1,4 @@
-insert into wniosek (id, typ, wzor) values (1, 3, null);
-insert into wniosek (id, typ, wzor) values (2, 1, null);
-insert into wniosek (id, typ, wzor) values (3, 2, null);
-insert into wniosek (id, typ, wzor) values (4, 3, null);
+insert into wniosek (id, typ, wzor, nazwa_obrazu) values (1, 3, null, null);
+insert into wniosek (id, typ, wzor, nazwa_obrazu) values (2, 1, null, null);
+insert into wniosek (id, typ, wzor, nazwa_obrazu) values (3, 2, null, null);
+insert into wniosek (id, typ, wzor, nazwa_obrazu) values (4, 3, null, null);
